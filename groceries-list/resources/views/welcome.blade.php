54d448200df93a8ba3852b92a20d391d28b7f2d0
Fideline <fideline_bzn@yahoo.com>
2022-08-16 18:43:51 +0200

    changing app purpose

--- groceries-list/resources/views/welcome.blade.php
+++ groceries-list/resources/views/welcome.blade.php
@@ -8,14 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Groceries List</title>
+    <title>Presence List</title>
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div class="container mx-auto mt-5 p-3" x-data="userInit({{ Js::from($users) }})">
         <div class="my-5 text-gray-900">
-            Achats restants: 0
+            Nombre de presence: 0
         </div>
 
         @foreach($users as $user)
